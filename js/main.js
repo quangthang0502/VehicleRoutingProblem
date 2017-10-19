48d982508978f547a02ad2dfb91376fdfe76fcbe
quangthang0502 <quangthang050296@gmail.com>
2017-10-19 22:16:18 +0700

    fix all code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+    $("#fileRead").change(function () {
+       var reader = new FileReader();
+       reader.onload = function () {
+           var data = this.result;
+           var items = data.split("\n");
+           var lanThu = $("#lanThu").val();
+           run(items,lanThu);
+       };
+       reader.readAsText(this.files[0]);
+    });
+});
+
+function run(items,lanThu) {
 
     // ====================================================
     //     Các hàm cơ bản
@@ -11,17 +24,17 @@ $(document).ready(function () {
 
     function printCircle(x, y, color) {
         ctx.beginPath();
-        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        ctx.arc(x, y, 10, 0, 2 * Math.PI);
         ctx.fillStyle = color;
         ctx.fill();
     }
 
     function printLine(Form, To, color) {
         ctx.beginPath();
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 5;
         ctx.lineCap = 'square';
-        ctx.moveTo(Form.x + 250, Form.y + 250);
-        ctx.lineTo(To.x + 250, To.y + 250);
+        ctx.moveTo(Form.x*10, Form.y*10);
+        ctx.lineTo(To.x*10, To.y*10);
         ctx.strokeStyle = color;
         ctx.stroke();
     }
@@ -31,16 +44,24 @@ $(document).ready(function () {
         return ketQua;
     }
 
+    function tinhTong(thanhPhan) {
+        var tong = 0;
+        for(tt = 0;tt < thanhPhan.length; tt ++) {
+            tong = tong + thanhPhan[tt].tongQuangDuong;
+        }
+        return tong;
+    }
+
     class ThanhPho {
         constructor(maTP, x, y, hangCanCungCap) {
             this.maTP = maTP;
             this.x = x;
             this.y = y;
             this.hangCanCungCap = hangCanCungCap;
-            this.xf = 250 + x;
-            this.yf = 250 + y;
+            this.xf = parseInt(x)*10;
+            this.yf = parseInt(y)*10;
             ctx.beginPath();
-            ctx.arc(this.xf, this.yf, 5, 0, 2 * Math.PI);
+            ctx.arc(this.xf, this.yf, 10, 0, 2 * Math.PI);
             ctx.fillStyle = 'red';
             ctx.fill();
             this.daQua = false;
@@ -56,10 +77,10 @@ $(document).ready(function () {
         constructor() {
             this.x = 0;
             this.y = 0;
-            this.xf = 250;
-            this.yf = 250;
+            this.xf = 0;
+            this.yf = 0;
             ctx.beginPath();
-            ctx.arc(this.xf, this.yf, 5, 0, 2 * Math.PI);
+            ctx.arc(this.xf, this.yf, 20, 0, 2 * Math.PI);
             ctx.fillStyle = 'black';
             ctx.fill();
         }
@@ -117,7 +138,6 @@ $(document).ready(function () {
         xeVeKho (){
             var diemHienTai = this.diQua.length - 1;
             this.veKho = true;
-            printLine(this.diQua[diemHienTai], this.diQua[0], this.color);
             this.tongQuangDuong = this.tongQuangDuong + khoangCach(this.diQua[diemHienTai], this.diQua[0]);
             this.diQua.push(this.diQua[0]);
         }
@@ -130,29 +150,36 @@ $(document).ready(function () {
     var khoHang;
     var cacThanhPho = [];
     var diaDiem;
-    $('#button').click(function () {
-        sucChua = $('#sucChua').val();
-        diaDiem = $('#diaDiem').val();
-        ctx.clearRect(0, 0, 500, 500);
-        khoHang = new KhoHang();
-        for (i = 0; i < diaDiem; i++) {
-            x = Math.floor(Math.random() * (250 + 250) - 250);
-            y = Math.floor(Math.random() * (250 + 250) - 250);
-            hangCungCap = Math.floor(Math.random() * (50 - 1) + 1);
-            cacThanhPho[i] = new ThanhPho(i + 1, x, y, hangCungCap);
+
+    sucChua = items[0];
+    diaDiem = items[1];
+    ctx.clearRect(0, 0, 1000, 1000);
+    khoHang = new KhoHang();
+    var m = 0;
+    var n = 0;
+    for(i = 2;i < items.length; i++){
+        if(i < (parseInt(diaDiem)+2)){
+            var content = items[i].split(' ');
+            cacThanhPho[m] = new ThanhPho(content[1], content[2], content[3], 0);
+            m++;
+        } else {
+            var content = items[i].split(' ');
+            cacThanhPho[n].hangCanCungCap = content[1];
+            n++;
         }
-        var table = $('#thanhPho');
-        var text = '';
-        for (i = 0; i < cacThanhPho.length; i++) {
-            text = "<tr>"
-                + "<td>" + cacThanhPho[i].maTP + "</td>"
-                + "<td>" + cacThanhPho[i].x + "</td>"
-                + "<td>" + cacThanhPho[i].y + "</td>"
-                + "<td>" + cacThanhPho[i].hangCanCungCap + "</td>"
-                + "</tr>";
-            table.append(text);
-        }
-    });
+    }
+    console.log(cacThanhPho);
+    var table = $('#thanhPho');
+    var text = '';
+    for (i = 0; i < cacThanhPho.length; i++) {
+        text = "<tr>"
+            + "<td>" + cacThanhPho[i].maTP + "</td>"
+            + "<td>" + cacThanhPho[i].x + "</td>"
+            + "<td>" + cacThanhPho[i].y + "</td>"
+            + "<td>" + cacThanhPho[i].hangCanCungCap + "</td>"
+            + "</tr>";
+        table.append(text);
+    }
 
     // =========================================================
     //     Thuật toán
@@ -162,75 +189,62 @@ $(document).ready(function () {
     var danhSachXe = [];
     var toiUu = [];
     var thanhPho;
-
     var ketqua = $('#ketqua');
 
-    function tinhTong(thanhPhan) {
-        var tong = 0;
-        for(tt = 0;tt < thanhPhan.length; tt ++) {
-            tong = tong + thanhPhan[i].tongQuangDuong;
+    for(thu = 0;thu < lanThu;thu++) {
+        thanhPho = cacThanhPho.slice();
+        while(true) {
+            var taoXe = new xeChoHang(sucChua, khoHang);
+            var x = Math.floor(Math.random() * thanhPho.length);
+            taoXe.checkHang(thanhPho[x]);
+            thanhPho.splice(x,1);
+            var a;
+            var y = true;
+            if(thanhPho.length == 0) {
+                taoXe.xeVeKho();
+                danhSachXe.push(taoXe);
+                break;
+            }
+            while(y != false) {
+                a = taoXe.diemGanNhat(thanhPho);
+                if (thanhPho.length == 0) {
+                    taoXe.xeVeKho();
+                    break;
+                }
+                y = taoXe.checkHang(thanhPho[a]);
+                if(y == true) {
+                    thanhPho.splice(a,1);
+                }
+            }
+            danhSachXe.push(taoXe);
+            if(thanhPho.length == 0) {
+                break;
+            }
         }
-        return tong;
+        if (thu != 0) {
+            if(tinhTong(toiUu) > tinhTong(danhSachXe)) {
+                toiUu = [];
+                toiUu = danhSachXe.slice();
+            }
+        } else {
+            toiUu = danhSachXe.slice();
+        }
+        danhSachXe = [];
     }
 
-    $('#toiUu').click(function () {
-        for(thu = 0;thu < 100;thu++) {
-            thanhPho = cacThanhPho.slice();
-            while(true) {
-                var taoXe = new xeChoHang(sucChua, khoHang);
-                var x = Math.floor(Math.random() * thanhPho.length);
-                taoXe.checkHang(thanhPho[x]);
-                thanhPho.splice(x,1);
-                var a;
-                var y = true;
-                if(thanhPho.length == 0) {
-                    taoXe.xeVeKho();
-                    danhSachXe.push(taoXe);
-                    break;
-                }
-                while(y != false) {
-                    a = taoXe.diemGanNhat(thanhPho);
-                    if (thanhPho.length == 0) {
-                        taoXe.xeVeKho();
-                        break;
-                    }
-                    y = taoXe.checkHang(thanhPho[a]);
-                    if(y == true) {
-                        thanhPho.splice(a,1);
-                    }
-                }
-                danhSachXe.push(taoXe);
-                if(thanhPho.length == 0) {
-                    break;
-                }
-            }
-            if (thu != 0) {
-                if(tinhTong(toiUu) > tinhTong(danhSachXe)) {
-                    toiUu = [];
-                    toiUu = danhSachXe.slice();
-                }
-            } else {
-                toiUu = danhSachXe.slice();
-                console.log(toiUu);
-            }
-            $('#cacBuoc').append("<p>Lan "+ (thu + 1) +" : " +tinhTong(danhSachXe)+"</p>");
-            danhSachXe = [];
+    ketqua.append("<h1>Tổng quãng đường : </h1>" + tinhTong(toiUu));
+    for(i = 0;i < toiUu.length;i++) {
+        var text = "<h3>Xe : "+ i +"</h3>";
+        ketqua.append(text);
+        var diQuaThanhPho = toiUu[i].diQua;
+        var texthead = "<ul class='thang'>";
+        for(j=1;j < diQuaThanhPho.length-1;j++){
+            texthead = texthead + "<li>"+diQuaThanhPho[j].maTP+"</li>";
         }
+        texthead = texthead + "</ul>";
+        ketqua.append(texthead);
+        ketqua.append("<p>Tổng quãng đường : </p>" + toiUu[i].tongQuangDuong);
+        toiUu[i].noiDiem();
+    }
 
-        ketqua.append("<h1>Tổng quãng đường : </h1>" + tinhTong(toiUu));
-        for(i = 0;i < toiUu.length;i++) {
-            var text = "<h3>Xe : "+ i +"</h3>";
-            ketqua.append(text);
-            var diQuaThanhPho = toiUu[i].diQua;
-            var texthead = "<ul class='thang'>";
-            for(j=1;j < diQuaThanhPho.length-1;j++){
-                texthead = texthead + "<li>"+diQuaThanhPho[j].maTP+"</li>";
-            }
-            texthead = texthead + "</ul>";
-            ketqua.append(texthead);
-            ketqua.append("<p>Tổng quãng đường : </p>" + toiUu[i].tongQuangDuong);
-            toiUu[i].noiDiem();
-        }
-
-    });
-});
+}
